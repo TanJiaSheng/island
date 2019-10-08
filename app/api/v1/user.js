af2db3445166b579d343b5aea984612dacc05bf6
tanjs <tjs_95@163.com>
2019-10-08 17:12:11 +0800

    密码加密

--- app/api/v1/user.js
+++ app/api/v1/user.js
@@ -1,6 +1,9 @@
+const bcrypt = require('bcryptjs')
 const KoaRouter = require('koa-router')
+
 const { RegisterValidator } = require('../../validators/validator')
 const { User } = require('../../models/user')
+
 const router = new KoaRouter( {
   prefix: '/v1/user'
 })
@@ -8,20 +11,22 @@ const router = new KoaRouter( {
 // 注册 新增数据 put get delete
 
 router.post('/register', async (ctx) => {
-  // 思维路径
-  // 接收参数 LinValidator
-  // email password1 password2 nickname
+  // 校验
   const v = await new RegisterValidator().validate(ctx)
+  // 加密
+  const salt = bcrypt.genSaltSync(10) // 10 位数，成本
+  const psw = bcrypt.hashSync(v.get('body.password1'), salt)
   // 获取数据
   // v.get
   const user = {
     email: v.get('body.email'),
-    password: v.get('body.password1'),
+    password: psw,
     nickname: v.get('body.nickname')
   }
-  // 数据库
+  // 插入数据库
   // SQL Model
   const r = await User.create(user)
+  // 返回数据给客户端
 })
 
 module.exports = router
